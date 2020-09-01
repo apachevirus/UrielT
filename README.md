a90a2a97b482e940744d5c6a878a59364fab19ac
Uriel <56313860+urltrn13@users.noreply.github.com>
2020-08-31 23:14:34 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# salesappi
+vinculacion sistema
---
+++ keys.js
@@ -0,0 +1,10 @@
+module.exports={
+
+    database:{
+        host:'localhost',
+        user:'figler',
+        password:'2523392',
+        database:'ieesystem'
+    }
+
+}
